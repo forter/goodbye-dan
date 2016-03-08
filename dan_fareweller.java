8dac7cc85a1346b0bd7e3b3f7c8e6b5595e82115
Re'em Bensimhon <reem@forter.com>
2016-03-08 16:53:44 +0200

    Create dan_fareweller.java

---
+++ dan_fareweller.java
@@ -0,0 +1,14 @@
+/**
+
+Dan,
+
+Even as someone who hadn't gotten to work with you much, your talets were still very noticeable. Intelligent, dilligent,  calm, mature and unintimidated by work. You will definitely be a huge asset to any project you'll work on.
+
+I'm certain you have a very bright future ahead of you. Make sure to have a lot of fun in the army, and who knows, maybe we'll get to work again at sometime in the future. I sure hope so.
+
+P.S.
+I didn't really write Java code here because I wanted to keep it short =)
+
+Re'em
+
+**/
